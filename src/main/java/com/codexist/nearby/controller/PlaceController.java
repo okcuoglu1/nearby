b3a4f38a44a22f5de8a458c071958fd45b8eb2b2
okcuoglu1 <okcuoglucemil@gmail.com>
2023-09-18 10:32:18 +0300

    The request class that will go to the Google Places API has been created and the GET method that meets the request from the user in the controller class has been created.

--- src/main/java/com/codexist/nearby/controller/PlaceController.java
+++ src/main/java/com/codexist/nearby/controller/PlaceController.java
@@ -1,14 +1,26 @@
 package com.codexist.nearby.controller;
 
+import com.codexist.nearby.payload.request.NearByRequest;
 import com.codexist.nearby.service.PlaceService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class PlaceController {
 
     private final PlaceService placeService;
+
+    @GetMapping("/nearby")
+    public ResponseEntity<> getNearbyPlaces(@RequestBody @Valid NearByRequest request) {
+        return placeService.getNearbyPlaces(request);
+    }
 
 }
---
+++ src/main/java/com/codexist/nearby/payload/request/NearByRequest.java
@@ -0,0 +1,30 @@
+package com.codexist.nearby.payload.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NearByRequest {
+
+    @Range(min = -90, max = 90, message = "Latitude must be between -90 and 90")
+    @NotNull(message = "Location latitude must be provided")
+    double latitude;
+
+    @Range(min = -180, max = 180, message = "Longitude must be between -180 and 180")
+    @NotNull(message = "Location longitude must be provided")
+    double longitude;
+
+    //message paket yap
+    @NotNull(message = "Radius must be provided")
+    @Positive(message = "Radius must be positive value")
+    int radius;
+
+}
