391c87d55f2dabaa0f6d59a0892646a6b9abfcb5
okcuoglu1 <okcuoglucemil@gmail.com>
2023-09-18 18:58:59 +0300

    Deploy Test

--- src/main/java/com/codexist/nearby/controller/PlaceController.java
+++ src/main/java/com/codexist/nearby/controller/PlaceController.java
@@ -8,12 +8,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/place")
 public class PlaceController {
 
     private final PlaceService placeService;
