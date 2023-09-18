d1b047265299186a565266b134cafd00d1703d13
okcuoglu1 <okcuoglucemil@gmail.com>
2023-09-18 11:25:56 +0300

    Created the place class to be saved in DB

--- src/main/java/com/codexist/nearby/entity/Place.java
+++ src/main/java/com/codexist/nearby/entity/Place.java
@@ -1,8 +1,15 @@
 package com.codexist.nearby.entity;
 
+import com.codexist.nearby.entity.google_response.Geometry;
+import com.codexist.nearby.entity.google_response.PlusCode;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.List;
 
 @Document(collection = "place")
 @Getter
@@ -11,5 +18,29 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @Builder(toBuilder = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Place {
-}
+public class Place implements Serializable {
+
+    @Id
+    String id;
+
+
+
+    private Geometry geometry;
+
+    private PlusCode plus_code;
+
+    private String business_status;
+    private String name;
+    private double rating;
+    private String reference;
+    private String scope;
+
+    private List<String> types;
+
+    private int user_ratings_total;
+    private String vicinity;
+    private String place_id;
+
+
+    @DBRef
+    List<Query> queries;
